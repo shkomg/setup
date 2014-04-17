19f8926a06f5f016c63c69d9374cc571b428b818
Bit Fund <bitfund.it@gmail.com>
2014-04-17 15:51:13 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,9 +17,9 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.24
-nvm use v0.10.24
-nvm alias default v0.10.24
+nvm install v0.10.26
+nvm use v0.10.26
+nvm alias default v0.10.26
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
