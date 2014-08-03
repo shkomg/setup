c2c8bd65302bc98b4fc9e07a78cc0c61653cb9cd
shkomg <serhiy.khvashchuk@gmail.com>
2014-08-03 21:49:48 +0300

    upgrade added

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y git
 sudo apt-get install -y curl
 
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y python-software-properties python g++ make
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
