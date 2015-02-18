ac25cd4db968d7f1753711bca81d368744a99862
shkomg <serhiy.khvashchuk@gmail.com>
2015-02-18 22:38:38 +0200

    update node version & delete heroku

--- setup.sh
+++ setup.sh
@@ -18,9 +18,9 @@ curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.26
-nvm use v0.10.26
-nvm alias default v0.10.26
+nvm install v0.12.0
+nvm use v0.12.0
+nvm alias default v0.12.0
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
@@ -38,7 +38,7 @@ sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
